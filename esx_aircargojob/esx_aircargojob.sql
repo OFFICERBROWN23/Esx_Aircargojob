f7bcc02266d500ec4dbc2fa6a5e16a5b28cbee2b
OFFICERBROWN23 <41652622+OFFICERBROWN23@users.noreply.github.com>
2018-11-05 14:18:06 -0500

    Add files via upload

---
+++ esx_aircargojob/esx_aircargojob.sql
@@ -0,0 +1,12 @@
+--
+-- jobs
+--
+
+INSERT INTO `jobs` (`name`, `label`) VALUES ('Heli', 'Worker')
+
+--
+-- grades
+--
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(NULL, 'Heli', 0, 'employee', 'Sling Operator', 200, '{"tshirt_1":15,"torso_1":48,"arms":17,"pants_1":46,"glasses_1":5,"decals_2":0,"hair_color_2":0,"helmet_2":0,"hair_color_1":0,"face":8,"glasses_2":0,"torso_2":0,"shoes":35,"hair_1":0,"skin":0,"sex":0,"glasses_1":5,"pants_2":1,"hair_2":0,"decals_1":0,"tshirt_2":0,"helmet_1":19}', '{"tshirt_1":36,"torso_1":48,"arms":17,"pants_1":46,"glasses_1":5,"decals_2":0,"hair_color_2":0,"helmet_2":0,"hair_color_1":0,"face":8,"glasses_2":0,"torso_2":0,"shoes":35,"hair_1":1,"skin":0,"sex":1,"glasses_1":5,"pants_2":1,"hair_2":0,"decals_1":0,"tshirt_2":0,"helmet_1":19}');
